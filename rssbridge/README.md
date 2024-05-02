fb3cf6cc656b1f3d01cfce3f4701dd8142cf6482
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2024-05-03 09:43:17 +1000

    samples for RSS Bridge

---
+++ rssbridge/README.md
@@ -0,0 +1,4 @@
+# RSS Bridge
+
+If the bridge and the reader are in the same docker network, can use a shortcut to reference rss bridge like below
+http://rss-bridge/?action=display&bridge=ABCNewsBridge&topic=qld&format=Atom
---
+++ rssbridge/config.ini.php
@@ -0,0 +1,17 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+
+[system]
+; How to enable all bridges: enabled_bridges[] = *
+enabled_bridges[] = ABCNewsBridge
+enabled_bridges[] = CssSelectorBridge
+enabled_bridges[] = FeedMerge
+enabled_bridges[] = FeedReducerBridge
+enabled_bridges[] = Filter
+enabled_bridges[] = GettrBridge
+enabled_bridges[] = MastodonBridge
+enabled_bridges[] = Reddit
+enabled_bridges[] = XPathBridge
+enabled_bridges[] = Youtube
+enabled_bridges[] = YouTubeCommunityTabBridge
+
+timezone = "Australia/Brisbane"
