2cdd0cd4b8db3600b9aa697f86691b949bd09bb9
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2024-12-19 18:27:11 +1000

    logging to power alerts

--- utils/alerts/src/alerts.py
+++ utils/alerts/src/alerts.py
@@ -1,9 +1,19 @@
+import logging
 import os
 import psutil
 import time
 
 from ntfy_connector import NtfyConnectorStd
 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.StreamHandler(),
+    ],
+)
 
 FIVE_MINUTES = 60 * 5
 
@@ -15,9 +25,11 @@ on_battery = False
 half_battery = False
 
 if __name__ == "__main__":
-    print(f"Begin checking status of device ({device_name=}, {topic=}, {check_frequency=})")
+    logging.info(f"Begin checking status of device ({device_name=}, {topic=}, {check_frequency=})")
     if not topic:
-        raise Exception("No NTFY topic defined, add ALERT_NTFY_TOPIC to env vars")
+        err_msg = "No NTFY topic defined, add ALERT_NTFY_TOPIC to env vars"
+        logging.exception(err_msg)
+        raise Exception(err_msg)
     ntfy_connector = NtfyConnectorStd(topic)
     while True:
         battery_status = psutil.sensors_battery()
@@ -28,6 +40,7 @@ if __name__ == "__main__":
         if not plugged_in and not on_battery:
             on_battery = True
             message = f"{device_name} on battery power now, {seconds_left}s left"
+            logging.info(message)
             ntfy_connector.push_notification(message)
         elif on_battery and not half_battery and percent < 50:
             half_battery = True
@@ -36,11 +49,13 @@ if __name__ == "__main__":
                 message += f", {int(seconds_left)}s left)"
             except:
                 message += ")"
+            logging.info(message)
             ntfy_connector.push_notification(message)
         elif plugged_in and on_battery:
             on_battery = False
             half_battery = False
             message = f"{device_name} on AC power now"
+            logging.info(message)
             ntfy_connector.push_notification(message)
 
         time.sleep(check_frequency)
