1de44136454983c050bf7eb8119a4b1361186973
MatthewTao <44959800+MatthewTao@users.noreply.github.com>
2024-12-19 18:01:18 +1000

    fix typo in alerts

--- utils/alerts/src/alerts.py
+++ utils/alerts/src/alerts.py
@@ -9,13 +9,13 @@ FIVE_MINUTES = 60 * 5
 
 device_name = os.environ.get("DEVICE_NAME", "testing")
 topic = os.environ.get("ALERT_NTFY_TOPIC")
-check_frequency = int(os.environ.get("CHECK_FREQEUNCY", FIVE_MINUTES))
+check_frequency = int(os.environ.get("CHECK_FREQUENCY", FIVE_MINUTES))
 
 on_battery = False
 half_battery = False
 
 if __name__ == "__main__":
-    print(f"Begin checking status of device ({device_name=}, {topic=})")
+    print(f"Begin checking status of device ({device_name=}, {topic=}, {check_frequency=})")
     if not topic:
         raise Exception("No NTFY topic defined, add ALERT_NTFY_TOPIC to env vars")
     ntfy_connector = NtfyConnectorStd(topic)
@@ -24,6 +24,7 @@ if __name__ == "__main__":
         plugged_in = battery_status.power_plugged
         seconds_left = battery_status.secsleft
         percent = battery_status.percent
+        # print(f"Checked {battery_status=}, {plugged_in=}, {seconds_left=}, {percent=}")
         if not plugged_in and not on_battery:
             on_battery = True
             message = f"{device_name} on battery power now, {seconds_left}s left"
